c57f3a4fbb9a258423114a0b55e970bbab214393
chaub123 <kinchau@hotmail.com.hk>
2025-04-15 22:11:52 +0800

    draft

---
+++ toolkit/src/test/java/com/aergia/toolkit/ToolkitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aergia.toolkit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolkitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
